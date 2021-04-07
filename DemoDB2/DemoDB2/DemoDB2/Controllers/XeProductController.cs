1e59bc39fab4f2657d34994c8861263ac0fa6417
luongdthong2889 <luong.dthong@gmail.com>
2021-04-07 18:56:01 +0700

    commit 6/4/2021
    commit 6/4/2021

---
+++ DemoDB2/DemoDB2/DemoDB2/Controllers/XeProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+using DemoDB2.Models;
+
+namespace DemoDB2.Controllers
+{
+    public class XeProductController : Controller
+    {
+        DBThueXeEntities db = new DBThueXeEntities();
+        // GET: XeProduct
+        public ActionResult Index()
+        {
+            return View(db.XEs.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
