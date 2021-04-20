ca3cc31148fd57fecd49bcb2781c470c7c641edc
luongdthong2889 <luong.dthong@gmail.com>
2021-04-20 15:25:45 +0700

    commit 20/4/2021
    commit 20/4/2021

---
+++ User/DoAn/DemoDB2/Models/Hang.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoDB2.Models
+{
+    public class Hang
+    {
+        public XE Xe { get; set; }
+        public int Soluong { get; set; }
+        public Hang(XE xe, int soluong)
+        {
+            Xe = xe;
+            Soluong = soluong;
+        }
+    }
+}
