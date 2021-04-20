69c6cd1f873f7ea1a4e9c86d9f6e729cb71e84b4
luongdthong2889 <luong.dthong@gmail.com>
2021-04-20 15:31:07 +0700

    commit 20/4/2021
    commit 20/4/2021

--- DemoDB/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DemoDB.MvcApplication" Language="C#" %>
---
+++ User/DoAn/DemoDB2/Controllers/TrangChuController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DemoDB2.Models;
+
+namespace DemoDB2.Controllers
+{
+    public class TrangChuController : Controller
+    {
+        DBThueXeEntities db = new DBThueXeEntities();
+        // GET: TrangChu
+        public ActionResult TrangChu()
+        {
+            return View(db.LOAIXEs.ToList());
+        }
+    }
+}
